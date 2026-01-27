5a4fb1de6024e290ebb3ee0a047fa7cd341b9c33
Ekta <ektasinha2023@gmail.com>
2026-01-27 12:05:29 +0530

    DSA - Sorting Algorithms

---
+++ DSA/06-Sorting_algoritms.py
@@ -0,0 +1,30 @@
+# Bubble sort -> O(n^2) time complexity
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]  # swap
+    return arr
+
+# Selection sort -> O(n^2) time complexity -> Select minimum and place it at correct position.
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+# Insertion sort -> O(n^2) time complexity -> Build sorted array one element at a time.
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
