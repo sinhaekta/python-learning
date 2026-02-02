f02df161f4f420bd80bc3e3307770f2b39457051
Ekta <ektasinha2023@gmail.com>
2026-02-02 12:06:10 +0530

    08 - Stacks: Basics

---
+++ DSA/08-Stacks.py
@@ -0,0 +1,27 @@
+# LIFO (Last In, First Out) - Stack data structure implementation
+# Use cases Undo/Redo, Expression evaluation, Backtracking algorithms
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("pop from empty stack")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("peek from empty stack")
+
+    def size(self):
+        return len(self.items)
