8b9095fced87627e23183c6968fdf5bfe2720e39
Ekta <ektasinha2023@gmail.com>
2026-03-05 14:43:48 +0530

    10-Linked_List

---
+++ DSA/10_LinkedLists.py
@@ -0,0 +1,30 @@
+# A Linked List is a linear data structure where elements are stored in nodes, and each node points to the next node.
+# Structure of a node: data + pointer to next node
+# Types of Linked Lists: Singly Linked List, Doubly Linked List, Circular Linked List
+
+# Python does not have a built-in Linked List class, so we create manually using classes
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data)
+            current_node = current_node.next
