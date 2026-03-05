bd2d62d1f9bc98e5b86c9c559826f11970b6af78
Ekta <ektasinha2023@gmail.com>
2026-03-05 15:14:08 +0530

    10-Linked_List

--- DSA/10_LinkedLists.py
+++ DSA/10_LinkedLists.py
@@ -46,3 +46,58 @@ class LinkedList:
         if not current_node:
             return
         prev.next = current_node.next
+
+class DoublyNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None    
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = DoublyNode(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+        new_node.prev = last_node
+
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data)
+            current_node = current_node.next
+
+    def insert_at_beginning(self, data):
+        new_node = DoublyNode(data)
+        new_node.next = self.head
+        if self.head:
+            self.head.prev = new_node
+        self.head = new_node
+    
+    def delete_node(self, key):
+        current_node = self.head
+        while current_node and current_node.data != key:
+            current_node = current_node.next
+        if not current_node:
+            return
+        if current_node.prev:
+            current_node.prev.next = current_node.next
+        else:
+            self.head = current_node.next  # Deleting head node
+        if current_node.next:
+            current_node.next.prev = current_node.prev
+
+# Example usage
+ll = LinkedList()
+ll.append(10)
+ll.append(20)       
+
+ll.insert_at_beginning(5)
+ll.print_list()
