6f551eb0a15e6de2742e343e3b3aa986749605c7
Ekta <ektasinha2023@gmail.com>
2026-03-10 11:22:23 +0530

    practice_ques: valid_parenthesis

---
+++ Practice_Questions/03_valid_parentheses.py
@@ -0,0 +1,13 @@
+def valid_parentheses(s: str) -> bool:
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+        if char in mapping:
+            top_element = stack.pop() if stack else '#'
+            if mapping[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+
+    return not stack
